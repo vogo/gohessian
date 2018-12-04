b99948f718b739c330021217e27bc7490402ac10
wangoo <wangoo@soundbus.cn>
2018-12-04 17:02:55 +0800

    update test

--- hessian_test.go
+++ hessian_test.go
@@ -44,39 +44,16 @@ type Worker struct {
 	Job []JOB
 }
 
-func equalName(n1, n2 *UserName) bool {
-	return n1 != nil && n2 != nil && n1.FirstName == n2.FirstName && n1.LastName == n2.LastName
-}
-
 func TestHessian(t *testing.T) {
 	name := UserName{
 		FirstName: "John",
 		LastName:  "Doe",
 	}
-	name2 := encodeDecode(name, t)
-	fmt.Println(reflect.TypeOf(name2))
-	fmt.Println(name2)
-
-	//	if !reflect.DeepEqual(&name, name2) {
-	//		t.Error("not equal from bytes")
-	//		t.FailNow()
-	//	}
-
-	fmt.Println("--------------")
 	person := Person{
 		UserName: name,
 		Age:      18,
 		Sex:      true,
 	}
-	person2 := encodeDecode(person, t)
-	fmt.Println(person2)
-
-	//	if !cmp.Equal(&person, person2) {
-	//		t.Error("not equal from bytes")
-	//		t.FailNow()
-	//	}
-
-	fmt.Println("--------------")
 	worker := Worker{
 		Person: person,
 		Job: []JOB{
@@ -84,18 +61,33 @@ func TestHessian(t *testing.T) {
 			JOB{Title: "ceo", Company: "microsoft"},
 		},
 	}
-	worker2 := encodeDecode(worker, t)
-	fmt.Println(worker2)
 
-	//	if !cmp.Equal(&worker, worker2) {
-	//		t.Error("not equal from bytes")
-	//		t.FailNow()
-	//	}
+	fmt.Println("worker:", worker)
+	typ := reflect.TypeOf(worker)
+	typMap := make(map[string]reflect.Type)
+	InitTypeMap(typ, typMap)
 
+	gh := NewGoHessian(typMap, nil)
+	fmt.Println("hessian:", gh)
+
+	bt, err := gh.ToBytes(worker)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	fmt.Println("bytes:", string(bt))
+	fmt.Println("bytes len:", len(bt))
+	res, err := gh.ToObject(bt)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	fmt.Println(res)
+	fmt.Println(reflect.TypeOf(res))
 }
 
-func GetObjectTypeMap(typ reflect.Type, typMap map[string]reflect.Type) {
-
+//InitTypeMap init
+func InitTypeMap(typ reflect.Type, typMap map[string]reflect.Type) {
 	fmt.Println("--------> ", typ.Name())
 	typMap[typ.Name()] = typ
 	for i := 0; i < typ.NumField(); i++ {
@@ -103,36 +95,12 @@ func GetObjectTypeMap(typ reflect.Type, typMap map[string]reflect.Type) {
 		fmt.Println(f.Type.Kind())
 		switch f.Type.Kind() {
 		case reflect.Struct:
-			GetObjectTypeMap(f.Type, typMap)
+			InitTypeMap(f.Type, typMap)
 		case reflect.Array:
 		case reflect.Slice:
 			if f.Type.Elem().Kind() == reflect.Struct {
-				GetObjectTypeMap(f.Type.Elem(), typMap)
+				InitTypeMap(f.Type.Elem(), typMap)
 			}
 		}
 	}
-}
-func encodeDecode(p interface{}, t *testing.T) interface{} {
-	fmt.Println("object:", p)
-	typ := reflect.TypeOf(p)
-	typMap := make(map[string]reflect.Type)
-	GetObjectTypeMap(typ, typMap)
-
-	gh := NewGoHessian(typMap, nil)
-	bt, err := gh.ToBytes(p)
-	fmt.Println("hessian:", gh)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-		return nil
-	}
-	fmt.Println("bytes:", string(bt))
-	fmt.Println("bytes len:", len(bt))
-	pnew, err := gh.ToObject(bt)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-		return nil
-	}
-	return pnew
 }
